4b70358e0c3c57ac961662258c7590232c5845f8
btdev0306 <btdev0306@outlook.com>
2024-06-17 09:28:55 -0300

    update

--- Typehints.py
+++ Typehints.py
@@ -1,3 +1,4 @@
+
 def get_full_name(first_name:str, last_name:str):
     full_name=first_name.title()+" "+last_name.title()
     return full_name
@@ -6,5 +7,19 @@ def get_name_with_age(name:str, age:int):
     name_with_age = name + "is this old: "+ str(age)
     return name_with_age
 
+def get_items(item_a: str, item_b: int, item_c: float, item_d: bool, item_e: bytes):
+    return item_a, item_b, item_c, item_d, item_d, item_e
+
+def process_items(items: list[str]):
+    for item in items:
+        print(item)
+
+class Person:
+    def __init__(self, name: str):
+        self.name = name
+
+
+def get_person_name(one_person: Person):
+    return one_person.name
 
 print(get_full_name("john","doe"))
